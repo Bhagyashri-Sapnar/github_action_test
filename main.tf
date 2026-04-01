4d35daa8ed967c986fb31f1930b0922a03414170
Bhagyashri-Sapnar <130666819+Bhagyashri-Sapnar@users.noreply.github.com>
2026-04-01 14:15:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
 
   tags = {
-    Owner = "Shrikat Dhanaade",
+    Owner = "Shrikat Dhanavade",
     Name  = "Test VM QA TEST"
   }
 }
